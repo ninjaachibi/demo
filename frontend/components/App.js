b92a134df62e27e2ac85fc29ef6c128eb8fcca32
Robert L <1998.robert@gmail.com>
2018-07-13 09:54:04 -0700

    first commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Welcome to React</h1>
+                <h1>Welcome to Rob's App</h1>
                 <p>
                     This is the App component.
                 </p>
